b0a0671faa3d1b9280914a9d9e68c69dc47283c4
Illyas <illyas.deulect@gmail.com>
2021-04-17 15:21:55 +0530

    added basic thing for navigation

--- Travel Helper/Travel_HelperApp.swift
+++ Travel Helper/Travel_HelperApp.swift
@@ -3,15 +3,17 @@
 //  Travel Helper
 //
 //  Created by admin on 17/04/21.
-//
-
+// this file like app.js in react native
+//this will run first
 import SwiftUI
 
 @main
 struct Travel_HelperApp: App {
     var body: some Scene {
         WindowGroup {
+            NavigationView{
             ContentView()
+            }
         }
     }
 }
