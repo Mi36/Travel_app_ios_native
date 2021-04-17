5cabe4b24c17d6f8e11edb9a977610f50d6258bf
Illyas <illyas.deulect@gmail.com>
2021-04-17 17:24:57 +0530

    added new screen

---
+++ Travel Helper/WorldView.swift
@@ -0,0 +1,20 @@
+//
+//  WorldView.swift
+//  Travel Helper
+//
+//  Created by admin on 17/04/21.
+//
+
+import SwiftUI
+import MapKit
+struct WorldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorldView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldView()
+    }
+}
